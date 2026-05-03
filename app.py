e70c5a5356bf9ff8d85580a4dbb6e021064e7f57
lmflex-ui <lmflexvideos@gmail.com>
2026-05-03 16:41:58 -0500

    switch to service account auth

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import json
 import anthropic
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from apscheduler.schedulers.background import BackgroundScheduler
